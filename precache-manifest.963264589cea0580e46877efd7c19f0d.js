95d041fff7a48fc7b7aee1b0024635e8071133e2
Zoriana <makieieva.dev@gmail.com>
2020-09-16 09:22:10 +0300

    Updates

---
+++ precache-manifest.963264589cea0580e46877efd7c19f0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0cecb4f902ab65e1b805c6a0d57c744",
+    "url": "/react-hw-01-components/index.html"
+  },
+  {
+    "revision": "93b1f44bc354c060c4c4",
+    "url": "/react-hw-01-components/static/css/main.61f30d70.chunk.css"
+  },
+  {
+    "revision": "7ec5cb0d3e87988c705e",
+    "url": "/react-hw-01-components/static/js/2.badfa631.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hw-01-components/static/js/2.badfa631.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93b1f44bc354c060c4c4",
+    "url": "/react-hw-01-components/static/js/main.8fa73625.chunk.js"
+  },
+  {
+    "revision": "6fe3cc52a8b0257f05d9",
+    "url": "/react-hw-01-components/static/js/runtime-main.459b7fff.js"
+  }
+]);
